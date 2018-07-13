6deabaf7790f65565618ad1bc4f73d27e1269fbb
Alicia Baker <aliciabaker314@gmail.com>
2018-07-13 15:17:10 -0600

    added user input box

--- Main.elm
+++ Main.elm
@@ -2,50 +2,75 @@ module Main exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events expsoing (..)
+import Html.Events exposing (onInput, onClick)
 
---model
-type alias Model =
-  Int
 
-initModel : Model
-initModel =
-  0
+-- model (what do we need to know)
+type alias Model
+  = { input: String
+    , totalCalories: Int
+    , errorMsg: String}
 
---Update
+-- update (what can happen)
 type Msg
-  = AddCalorie
-  | Clear
+    = AddCalorie
+    | Clear
+    | UserInput String
 
+
+-- (where stuff happens )
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    AddCalorie ->
-      model + 1
-    Clear ->
-      initModel
+    case msg of
+        AddCalorie ->
+            case String.toInt model.input of
+               Ok value -> {model | totalCalories = model.totalCalories + value, errorMsg = ""}
+               Err value -> {model | errorMsg = "Requires a Number"}
 
---Views
+
+        Clear ->
+          {model | totalCalories = 0, input = ""}
+
+        UserInput input ->
+          {model | input = input }
+
+
+
+initModel : Model
+initModel = {input = ""
+            , totalCalories = 0
+            , errorMsg = ""}
+
+
+
+-- view
+
+
 view : Model -> Html Msg
 view model =
-  div []
-  [ h3 []
-    [ text ("Total Calories: " ++ (toString model)) ]
-  , button
-    [ type_ "button"
-    , onClick AddCalorie
-    ]
-    [ text "Add"]
-  , button
-    [ type_ "button"
-  ,  onClick Clear]
-  ]
-  [ text "Clear"]
+    div []
+    [ Html.input [ onInput UserInput, value model.input][]
+        , h3 []
+            [ text ("Total Calories: " ++ (toString model.totalCalories )) ]
+        , button
+            [ type_ "button"
+            , onClick AddCalorie
+            ]
+            [ text "Add" ]
+        , button
+            [ type_ "button"
+            , onClick Clear
+            ]
+            [ text "Clear" ]
+        , div [ ]
+            [ text model.errorMsg ]
+        ]
+
 
 main : Program Never Model Msg
 main =
-  Html.beginnerProgram
-  { model = initModel
-  , update = update
-  , view = view
-  }
+    Html.beginnerProgram
+        { model = initModel
+        , update = update
+        , view = view
+        }
